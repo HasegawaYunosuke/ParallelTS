d57cf82b7fd80b92621db3f20ce16d4aadc1db63
Hasegawa Yunosuke <hasegawa.yunosuke@gmail.com>
2010-10-14 17:13:14 +0900

    プログラム作成開始。Gitもリモート設定済み。
    ＊課題
    まず動くものを作る

---
+++ header.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void init(int argc, char **argv); /*Initialize*/
+void cif(int argc, char **argv); /*Check Input-Files*/
+void mif(int argc, char **argv); /*Management Input-File*/
---
+++ read_data.c
@@ -0,0 +1,5 @@
+#include "header.h"
+
+void mif(int argc, char **argv)
+{
+}
