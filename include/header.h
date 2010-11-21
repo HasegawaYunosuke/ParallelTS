d4eb646c0bddfe477027d23974dddd49d9226cae
Hasegawa Yunosuke <hasegawa.yunosuke@gmail.com>
2010-11-22 04:13:58 +0900

    最近近傍法による初期解生成を実装。以下TODO
    ＊TODO
        タブーサーチの実装

--- include/header.h
+++ include/header.h
@@ -58,6 +58,10 @@ void ptp(void); /* pthread_Procedure */
                 void cuc_pa(int * ary, int target, int max, int flag); /* Check Used-City and Plug Array */
                 double gpd(int * path, int * npthr); /* Locked:Get Path-Distance */
             void cip_bnn(int * npthr); /* Create Initial-Path By Nearest Neighbor */
+                int gnc_eu(int sc, int * yet, int surplus); /* Get Nearest City Except Used Cities */
+                    int is_u(int city, int * ary, int max); /* Is This Used City? (return YES/NO) */
+                int guci_bc(int city, int * ary, int max); /* Get Used City Index By City */
+
 void fin(void); /* Finalize */
     void fnyfp(void); /* Free Not Yet Freed Pointer */
 #ifdef MPIMODE
--- src/init_path.c
+++ src/init_path.c
@@ -45,4 +45,70 @@ void cuc_pa(int * ary, int max, int target, int flag)
 
 void cip_bnn(int * npthr)
 {
+    int i;
+    int cc = grn(g_bd.ps) + 1; /* Choiced City */
+    int ci; /* Choiced Index (at array:yet[]) */
+    int ucf = -1; /* Used City Flag */
+    int yet[g_bd.ps]; /* Not yet used Cities */
+    double dis; /* All Distance of Current Sol-Path */
+
+    for(i = 0; i < g_bd.ps; i++) {
+        yet[i] = i + 1;
+    }
+
+    ig_p[*npthr].csp[0] = cc;
+    cuc_pa(yet, g_bd.ps, cc - 1, ucf);
+
+    for(i = 1; i < g_bd.ps; i++) {
+        cc = gnc_eu(cc, yet, (g_bd.ps - i));
+        ig_p[*npthr].csp[i] = cc;
+        ci = guci_bc(cc, yet, (g_bd.ps - i));
+        cuc_pa(yet, (g_bd.ps - i), ci, ucf);
+    }
+
+    ig_p[*npthr].csd = gpd(ig_p[*npthr].csp, npthr);
+}
+
+int gnc_eu(int sc, int * yet, int surplus)
+{
+    int eci; /* End City Index */
+    int min_ci; /* Minimum City Index */
+    double min = DBL_MAX;
+
+    for(eci = 0; eci < g_bd.ps; eci++) {
+        if(g_bd.bgd[sc - 1][eci] != 0.0 &&
+            g_bd.bgd[sc - 1][eci] < min &&
+                is_u(eci + 1, yet, surplus) == NO) {
+            min = g_bd.bgd[sc - 1][eci];
+            min_ci = eci;
+        }
+    }
+
+    return (min_ci + 1);
+}
+
+int is_u(int city, int * ary, int max)
+{
+    int i;
+
+    for(i = 0; i < max; i++) {
+        if(ary[i] == city) {
+            return NO;
+        }
+    }
+
+    return YES;
+}
+
+int guci_bc(int city, int * ary, int max)
+{
+    int i;
+
+    for(i = 0; i < max; i++) {
+        if(ary[i] == city) {
+            return i;
+        }
+    }
+
+    oem("guci_bc", "Couldn't Find Out city", city);
 }
