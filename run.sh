3c7cad845473fdbba46c7e8184f408590ed0a00c
Hasegawa Yunosuke <share@Google.(none)>
2010-11-27 20:53:52 +0900

    動作確認を完了。初期解生成をnn、スレッド数2以下なら確実に大丈夫。以下詳細
    ＊確認実験
        まず探索時間について、都市数575の問題を、
        10sec ～ 3600sec の範囲で、スレッド数2で動作確認。成功
        また問題数について、
        51、318、575、1000くらい、4000くらい、10000くらいで動作確認。成功
        最後にシェルによる連続実行の動作確認。成功
    
    ＊TODO
        ・コアへの明示的なスレッドの割り振り
        （なお、搭載されるコア数からではなく、設定ファイルのスレッド数のみで決定すべきと考えている）
        ・多様性オペレータの実装

--- run.sh
+++ run.sh
@@ -6,5 +6,9 @@ scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/test.set
 fi
 if [ $1 == 2 ]
 then
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/test1.set
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/eil51_300sec.set
+sleep 10
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/lin318_300sec.set
+sleep 10
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_300sec.set
 fi
