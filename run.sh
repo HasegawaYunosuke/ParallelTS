0922c4b1420e925e2457419c226623f8a26ac26f
Hasegawa Yunosuke <share@Google.(none)>
2010-11-28 09:42:00 +0900

    プログラムには変更無し。入力する設定ファイルに変更を加え、MP_Send()実装を開始。以下詳細。
    ＊入力ファイルのフォーマット変更
        現在入力設定ファイルは16行目に「p_wloop:」、改悪許容回数を読み込み、
        また入力設定ファイルの19行目に「p_TLT」、タブー期間をそれぞれ読み込んでいる。
        それぞれ意味は異なるが、
    
            タブーリストのサイズ ＝ タブー期間
    
        となっている以上、タブー期間には意味が無い。
        また動作確認実験の様子からも問題が無さそうなので、
        19行目の「p_TLT」の項目を削除する。
        （まだ消して無い！）
    
    ＊MPI_Send()実装の開始
        上記の入力設定ファイルフォーマットの変更は、
        MPI_Send()の実装を行う上で、「どのタイミングで送受信を行うか？」
        という点で重要であり、この送受信タイミングとして、
        改悪許容回数を取り入れようと考えている。
    
        ここで重要なのが、「改悪許容回数」というパラメータの意味であり、
        「改悪が許される回数」ではなく、正確には
        「解の更新が連続して行われないでも許される回数」である、というかそうする。
    
        つまり、このp_wloop回数連続で解の更新（つまりその時点での最も良い解を上回る解への遷移）
        が行われ「なかった」場合、MPIによる情報の送受信が行われる、という形式にする。
        （もちろんそんなのまだ作ってない！）
    
    ＊TODO
        上記項目について、やや大幅な変更ということで、betaブランチに移動する。

--- run.sh
+++ run.sh
@@ -2,7 +2,23 @@
 
 if [ $1 == 1 ]
 then
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/test.set
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_1h_0005per.set
+sleep 10
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_1h_0005per.set
+sleep 10
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_1h_0005per.set
+sleep 10
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_1h_005per.set
+sleep 10
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_1h_005per.set
+sleep 10
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_1h_005per.set
+sleep 10
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_1h_05per.set
+sleep 10
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_1h_05per.set
+sleep 10
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_1h_05per.set
 fi
 if [ $1 == 2 ]
 then
