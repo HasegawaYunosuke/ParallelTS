ffaf7ee3123eb58f40162c591acca661f2b738f5
Hasegawa Yunosuke <hasegawa.yunosuke@gmail.com>
2011-01-10 00:58:58 +0900

    一層不要なファイルを削除

--- lun2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scrun -group=nop2_2,network=sctp -nodes=2x1 ./b.out input/GA_rat575.set
--- lun4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scrun -group=nop2_4,network=sctp -nodes=2x1 ./b.out input/GA_rat575.set
-sleep 30
-scrun -group=nop2_4,network=sctp -nodes=2x1 ./b.out input/GA_rat575.set
