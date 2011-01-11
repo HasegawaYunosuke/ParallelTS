2ea3d1cdf140db42c9a614ed696c1159e41ffe03
Hasegawa Yunosuke <share@Google.(none)>
2011-01-11 17:19:14 +0900

    情報操作用誘爆実験

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 if [ $1 == 1 ]
 then
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_pthr1.set
-sleep 30
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_pthr4.set
+scrun -group=pc4_1,network=sctp -nodes=4x1 ./b.out input/GA_rat575.set &
+sleep 10
+scrun -group=pc4_2,network=sctp -nodes=4x1 ./b.out input/GA_rat575.set
 fi
 if [ $1 == 2 ]
 then
