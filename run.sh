b3edc22675dddd2f61eda2535c846734fe7ea7a1
Hasegawa Yunosuke <share@Google.(none)>
2011-01-11 23:32:08 +0900

    ログ情報を追加

--- run.sh
+++ run.sh
@@ -2,11 +2,15 @@
 
 if [ $1 == 1 ]
 then
-scrun -group=pc4_1,network=sctp -nodes=4x1 ./b.out input/GA_rat575.set &
-sleep 10
-scrun -group=pc4_2,network=sctp -nodes=4x1 ./b.out input/GA_rat575.set
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_pthr1.set
+sleep 30
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_pthr2.set
+sleep 30
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_pthr4.set
+sleep 30
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/Long_pr2392.set
 fi
 if [ $1 == 2 ]
 then
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/dsj1000_long_log.set
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/test.set
 fi
