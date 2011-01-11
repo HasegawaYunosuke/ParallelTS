7fb4b83ff2b4f0110c31c493458be85e6110ac5e
Hasegawa Yunosuke <share@Google.(none)>
2011-01-11 15:11:41 +0900

    並列手法でのスレッド数別探索（rat575）のログを追加

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 
 if [ $1 == 1 ]
 then
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/Long_rat575.set
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_pthr1.set
+sleep 30
+scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_pthr4.set
 fi
 if [ $1 == 2 ]
 then
