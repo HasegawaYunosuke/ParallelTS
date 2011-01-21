fdb48e0a7ff7ff71540b41d79faed0299588e5dd
Hasegawa Yunosuke <share@Google.(none)>
2011-01-22 06:30:29 +0900

    dsj1000のノード数別実験の結果を追加

--- run.sh
+++ run.sh
@@ -2,13 +2,83 @@
 
 if [ $1 == 1 ]
 then
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_pthr1.set
+scrun -group=pc11_12,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc13_14,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc15_16,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
 sleep 30
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_pthr2.set
+scrun -group=pc11_12,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc13_14,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc15_16,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
 sleep 30
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_pthr4.set
+
+scrun -group=pc11_14,network=sctp -nodes=4x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc15_18,network=sctp -nodes=4x1 ./b.out input/GA_dsj1000.set
 sleep 30
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/Long_pr2392.set
+scrun -group=pc11_14,network=sctp -nodes=4x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc15_18,network=sctp -nodes=4x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_14,network=sctp -nodes=4x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc15_18,network=sctp -nodes=4x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_14,network=sctp -nodes=4x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc15_18,network=sctp -nodes=4x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_14,network=sctp -nodes=4x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc15_18,network=sctp -nodes=4x1 ./b.out input/GA_dsj1000.set
+sleep 30
+
+scrun -group=pc11_16,network=sctp -nodes=6x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_16,network=sctp -nodes=6x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_16,network=sctp -nodes=6x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_16,network=sctp -nodes=6x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_16,network=sctp -nodes=6x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_16,network=sctp -nodes=6x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_16,network=sctp -nodes=6x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_16,network=sctp -nodes=6x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_16,network=sctp -nodes=6x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
+sleep 30
+scrun -group=pc11_16,network=sctp -nodes=6x1 ./b.out input/GA_dsj1000.set &
+sleep 10
+scrun -group=pc17_18,network=sctp -nodes=2x1 ./b.out input/GA_dsj1000.set
 fi
 if [ $1 == 2 ]
 then
