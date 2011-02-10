048d99b4d1f9220fa996c5fcb2be32adb211c902
Hasegawa Yunosuke <share@Google.(none)>
2011-02-10 09:01:36 +0900

    結果途中結果

--- run.sh
+++ run.sh
@@ -24,28 +24,6 @@ scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/NO_hum_dsj1000.se
 fi
 if [ $1 == 2 ]
 then
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr1_DEBUG.set
-sleep 10
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr2_DEBUG.set
-sleep 10
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr4_DEBUG.set
-sleep 10
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr1.set
-sleep 10
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr2.set
-sleep 10
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr4.set
-sleep 10
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr1.set
-sleep 10
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr2.set
-sleep 10
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr4.set
-sleep 10
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr1.set
-sleep 10
-scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr2.set
-sleep 10
 scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr4.set
 sleep 10
 scrun -group=machinefile,network=sctp -nodes=8x1 ./b.out input/rat575_MPI_pthr1.set
